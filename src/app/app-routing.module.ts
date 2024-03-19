ed4417a70bf152fc892a7e389007f89c8ecbdc66
Robert-Martins <robertcantares@gmail.com>
2024-03-19 19:11:21 -0300

    feat/declare-app-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { TicTacToeComponent } from './view/modes/tic-tac-toe/tic-tac-toe.component';
+import { SuperTicTacToeComponent } from './view/modes/super-tic-tac-toe/super-tic-tac-toe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'game',
+    children: [
+      {
+        path: 'tic-tac-toe',
+        component: TicTacToeComponent
+      },
+      {
+        path: 'super-tic-tac-toe',
+        component: SuperTicTacToeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
